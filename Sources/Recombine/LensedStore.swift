912160c83586c3e307813f8460a0e65336a2c6ce
Mario Uher <uher.mario@gmail.com>
2022-03-24 12:29:45 +0100

    Add more complex tests

--- Sources/Recombine/LensedStore.swift
+++ Sources/Recombine/LensedStore.swift
@@ -4,22 +4,22 @@ import Combine
 public class LensedStore<Action, StoreState, LensedStoreState>: ObservableObject, Recombine {
   public init(_ store: Store<Action, StoreState>, keyPath: KeyPath<StoreState, LensedStoreState>) {
     self.store = store
-    self.state = store.state[keyPath: keyPath]
+    self.value = store.state[keyPath: keyPath]
 
     self.cancellable = store.$state.sink {
-      self.state = $0[keyPath: keyPath]
+      self.value = $0[keyPath: keyPath]
     }
   }
 
   public subscript<T>(dynamicMember keyPath: KeyPath<LensedStoreState, T>) -> T {
-    return state[keyPath: keyPath]
+    return value[keyPath: keyPath]
   }
 
   public func dispatch(_ action: Action) {
     store.dispatch(action)
   }
 
-  @Published private(set) var state: LensedStoreState
+  @Published private(set) var value: LensedStoreState
 
   private let store: Store<Action, StoreState>
   private var cancellable: AnyCancellable?
--- Tests/RecombineTests/RecombineTests.swift
+++ Tests/RecombineTests/RecombineTests.swift
@@ -6,31 +6,36 @@ import XCTest
 class RecombineTests: XCTestCase {
   func testInitialState() {
     let store = AppStore(
-      initialState: State(flag: false),
+      initialState: State(),
       reducers: []
     )
 
     XCTAssertEqual(store.counter, 0)
     XCTAssertFalse(store.flag)
+    XCTAssertEqual(store.user, User(username: "Alice"))
+    XCTAssertEqual(store.user.username, "Alice")
   }
 
   func testReducers() {
     let store = AppStore(
-      initialState: State(flag: false),
-      reducers: [counterReducer]
+      initialState: State(),
+      reducers: [reducer]
     )
 
     store.dispatch(.increase)
     store.dispatch(.decrease)
     store.dispatch(.decrease)
+    store.dispatch(.rename("Bob"))
 
     XCTAssertEqual(store.counter, -1)
     XCTAssertFalse(store.flag)
+    XCTAssertEqual(store.user.username, "Bob")
 
     store.dispatch(.toggle)
 
     XCTAssertEqual(store.counter, -1)
     XCTAssertTrue(store.flag)
+    XCTAssertEqual(store.user.username, "Bob")
   }
 
   func testMiddlewares() {
@@ -67,8 +72,8 @@ class RecombineTests: XCTestCase {
     }
 
     let store = AppStore(
-      initialState: State(flag: false),
-      reducers: [counterReducer],
+      initialState: State(),
+      reducers: [reducer],
       middlewares: [asyncMiddleware, syncMiddleware]
     )
 
@@ -80,11 +85,13 @@ class RecombineTests: XCTestCase {
 
     XCTAssertEqual(store.counter, -3)
     XCTAssertFalse(store.flag)
+    XCTAssertEqual(store.user.username, "Bob")
 
     store.dispatch(.toggle)
 
     XCTAssertEqual(store.counter, -3)
     XCTAssertTrue(store.flag)
+    XCTAssertEqual(store.user.username, "Bob")
   }
 
   func testDispatchingMiddlewares() {
@@ -109,8 +116,8 @@ class RecombineTests: XCTestCase {
     }
 
     let store = AppStore(
-      initialState: State(flag: false),
-      reducers: [counterReducer],
+      initialState: State(),
+      reducers: [reducer],
       middlewares: [dispatchingMiddleware]
     )
 
@@ -123,8 +130,8 @@ class RecombineTests: XCTestCase {
 
   func testSubStateLensing() {
     let store = AppStore(
-      initialState: State(flag: false),
-      reducers: [counterReducer]
+      initialState: State(),
+      reducers: [reducer]
     )
 
     XCTAssertEqual(store.counter, 0)
@@ -132,35 +139,47 @@ class RecombineTests: XCTestCase {
 
     let counterLense = store.lense(\.counter)
     let flagLense = store.lense(\.flag)
+    let usernameLense = store.lense(\.user.username)
 
-    XCTAssertEqual(counterLense.state, 0)
-    XCTAssertFalse(flagLense.state)
+    XCTAssertEqual(counterLense.value, 0)
+    XCTAssertFalse(flagLense.value)
+    XCTAssertEqual(usernameLense.value, "Alice")
 
     store.dispatch(.increase)
+    store.dispatch(.toggle)
+    store.dispatch(.rename("Bob"))
 
-    XCTAssertEqual(counterLense.state, 1)
-    XCTAssertFalse(flagLense.state)
+    XCTAssertEqual(counterLense.value, 1)
+    XCTAssertTrue(flagLense.value)
+    XCTAssertEqual(usernameLense.value, "Bob")
 
-    XCTAssertEqual(store.counter, counterLense.state)
-    XCTAssertEqual(store.flag, flagLense.state)
+    XCTAssertEqual(store.counter, counterLense.value)
+    XCTAssertEqual(store.flag, flagLense.value)
+    XCTAssertEqual(store.user.username, usernameLense.value)
   }
 }
 
 extension RecombineTests {
   struct State: Changeable {
     var counter = 0
-    var flag: Bool
+    var flag = false
+    var user = User(username: "Alice")
+  }
+
+  struct User: Equatable {
+    var username: String
   }
 
   enum Action {
     case increase
     case decrease
     case toggle
+    case rename(String)
   }
 
   typealias AppStore = Store<Action, State>
 
-  func counterReducer(action: Action, state: State) -> State {
+  func reducer(action: Action, state: State) -> State {
     switch action {
     case .increase:
       return state.change(\.counter, to: state.counter + 1)
@@ -168,6 +187,10 @@ extension RecombineTests {
       return state.change(\.counter, to: state.counter - 1)
     case .toggle:
       return state.change(\.flag, to: !state.flag)
+    case let .rename(username):
+      var user = state.user
+      user.username = username
+      return state.change(\.user, to: user)
     }
   }
 }
